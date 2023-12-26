9e7f1542db7935115bee25de27f9a0f0d5fa5706
marcosdavidev <adamsdavi77@gmail.com>
2023-12-26 11:38:24 -0300

    Movendo peças

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -25,24 +25,37 @@ public class Board {
 
 	public Piece piece(int row, int column){
 		if(!positionExists(row, column)){
-			throw new BoardException("Posição não está no tabuleiro");
+			throw new BoardException("Posicao nao existe no tabuleiro");
 		}
 		return pieces[row][column];
 	}
 	
 	public Piece piece(Position position){
 		if(!positionExists(position)){
-			throw new BoardException("Posição não está no tabuleiro");
+			throw new BoardException("Posicao nao existe no tabuleiro");
 		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
 	public void placePiece(Piece piece, Position position){
 		if(thereIsAPiece(position)){
-			throw new BoardException("Já existe uma peça nesta posição " + position);
+			throw new BoardException("Ja existe uma peça nesta posicao " + position);
 		}
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+	
+	public Piece removePiece(Position position){
+		if(!positionExists(position)){
+			throw new BoardException("Posicao nao existe no tabuleiro"); 
+		}
+		if(piece(position) == null ){
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position=null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
 	}
 	
 	private boolean positionExists(int row, int column){
@@ -55,7 +68,7 @@ public class Board {
 	
 	public boolean thereIsAPiece(Position position){
 		if(!positionExists(position)){
-			throw new BoardException("Posição não está no tabuleiro");
+			throw new BoardException("Posicao nao existe no tabuleiro");
 		}
 		return piece(position) != null;
 	}
